d1cb38d358638ee10b18509fdfa6db0ee949cbd3
Charis <bob@peershipapp.com>
2019-07-19 16:45:22 +0300

    hide firebase env setup

--- src/Firestore.js
+++ src/Firestore.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 const config = {
-    apiKey: "AIzaSyDwya7cQ2-NG4QFO99zg-66prX5WNl7K4w",
-    authDomain: "shift-400bc.firebaseapp.com",
-    databaseURL: "https://shift-400bc.firebaseio.com",
-    projectId: "shift-400bc",
+    apiKey: "apiKey",
+    authDomain: "authDomain",
+    databaseURL: "databaseURL",
+    projectId: "projectId",
     storageBucket: "",
-    messagingSenderId: "998087206498",
-    appId: "1:998087206498:web:608d8416cae6e8a8"
+    messagingSenderId: "messagingSenderId",
+    appId: "appId"
 };
 firebase.initializeApp(config);
 export default firebase;
